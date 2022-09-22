27f6d600f71cc068c27acb7731118836f02dd7ca
Stuart George <stuart@accentdesign.co.uk>
2022-09-22 12:28:01 +0100

    fix root test

--- src/tests/api/endpoints/test_root.py
+++ src/tests/api/endpoints/test_root.py
@@ -1,24 +1,9 @@
 import pytest
 from starlette import status
 
-from app.database.tables import User
-
 pytestmark = pytest.mark.asyncio
 
 
-async def test_no_token_no_access(client):
+async def test_ok(client):
     response = await client.get("/")
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-
-async def test_inactive_no_access(client, user, db, login_token):
-    await db.update(User, user.id, {"is_active": False})
-    headers = {"Authorization": f"bearer {login_token}"}
-    response = await client.get("/", headers=headers)
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-
-async def test_ok(client, login_token):
-    headers = {"Authorization": f"bearer {login_token}"}
-    response = await client.get("/", headers=headers)
     assert response.status_code == status.HTTP_200_OK
