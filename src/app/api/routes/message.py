30f2000c4c61b2a00bfb49216240a6b843c6e250
Stuart George <stuart@accentdesign.co.uk>
2022-09-22 13:37:01 +0100

    add message delete endpoint

--- src/app/api/routes/message.py
+++ src/app/api/routes/message.py
@@ -1,8 +1,10 @@
 from typing import List
+from uuid import UUID
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.responses import Response
 
 from app.api.schemas import MessageRead
 from app.database.session import get_session
@@ -15,3 +17,28 @@ router = APIRouter()
 async def list_messages(session: AsyncSession = Depends(get_session)):
     statement = select(Message)
     return list((await session.execute(statement)).scalars())
+
+
+@router.get("/{id}", response_model=MessageRead)
+async def get_messages(
+    id: UUID,
+    session: AsyncSession = Depends(get_session)
+):
+    statement = select(Message).where(Message.id == id)
+    message = (await session.execute(statement)).scalar()
+    if not message:
+        raise HTTPException(404)
+    return message
+
+
+@router.delete("/{id}", response_class=Response, status_code=204)
+async def delete_messages(
+    id: UUID,
+    session: AsyncSession = Depends(get_session)
+):
+    statement = select(Message).where(Message.id == id)
+    message = (await session.execute(statement)).scalar()
+    if not message:
+        raise HTTPException(404)
+    await session.delete(message)
+    await session.commit()
