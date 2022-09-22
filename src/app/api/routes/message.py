9b0db09f047cdefc04e161b0ab4b3fc05bde90b2
Stuart George <stuart@accentdesign.co.uk>
2022-09-22 13:45:57 +0100

    refactor meessage to dependency

--- src/app/api/routes/message.py
+++ src/app/api/routes/message.py
@@ -1,11 +1,11 @@
 from typing import List
-from uuid import UUID
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.responses import Response
 
+from app.api.dependencies import get_message
 from app.api.schemas import MessageRead
 from app.database.session import get_session
 from app.database.tables import Message
@@ -21,24 +21,15 @@ async def list_messages(session: AsyncSession = Depends(get_session)):
 
 @router.get("/{id}", response_model=MessageRead)
 async def get_messages(
-    id: UUID,
-    session: AsyncSession = Depends(get_session)
+    message: Message = Depends(get_message)
 ):
-    statement = select(Message).where(Message.id == id)
-    message = (await session.execute(statement)).scalar()
-    if not message:
-        raise HTTPException(404)
     return message
 
 
 @router.delete("/{id}", response_class=Response, status_code=204)
 async def delete_messages(
-    id: UUID,
+    message: Message = Depends(get_message),
     session: AsyncSession = Depends(get_session)
 ):
-    statement = select(Message).where(Message.id == id)
-    message = (await session.execute(statement)).scalar()
-    if not message:
-        raise HTTPException(404)
     await session.delete(message)
     await session.commit()
