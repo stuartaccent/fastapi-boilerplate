986f992577a2541a7959efa0f9c3458f87af1243
Stuart George <stuart@accentdesign.co.uk>
2023-04-26 13:05:16 +0100

    add the create user typer command

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,71 @@
+from asyncio import run as aiorun
+
+import grpc
 import typer
 
+from app.config import settings
+from app.grpc import AuthGrpcClient
+from protos import auth_pb2
+
 app = typer.Typer()
+
+
+async def _create_user(
+    email: str,
+    first_name: str,
+    last_name: str,
+    password: str,
+    verified: bool,
+):
+    timeout = 5
+    async with AuthGrpcClient(settings.auth_host, settings.auth_port) as client:
+        try:
+            # create the user
+            request = auth_pb2.RegisterRequest(
+                email=email,
+                first_name=first_name,
+                last_name=last_name,
+                password=password,
+            )
+            user = await client.Register(request, timeout=timeout)
+            print(f"user created: {user.email}")
+            # if verified
+            if verified:
+                # request a token
+                request = auth_pb2.VerifyUserTokenRequest(email=user.email)
+                token = await client.VerifyUserToken(request, timeout=timeout)
+                print(f"verification token: {token.token}")
+                # verify
+                request = auth_pb2.Token(token=token.token)
+                verify = await client.VerifyUser(request, timeout=timeout)
+                print(f"user verified: {verify.is_verified}")
+
+        except grpc.aio.AioRpcError as e:
+            raise e
+
+
+@app.command()
+def create_user(
+    email: str,
+    first_name: str,
+    last_name: str,
+    password: str,
+    verified: bool = False,
+):
+    coro = _create_user(
+        email,
+        first_name,
+        last_name,
+        password,
+        verified,
+    )
+    aiorun(coro)
+    print("completed")
+
+
+@app.command()
+def hello():
+    print("hello")
 
 
 if __name__ == "__main__":
