f553f0176b515292f047de3a0797a90e369492c2
Stuart George <stuart@accentdesign.co.uk>
2023-07-25 09:51:02 +0100

    update dependencies

--- src/app/database/engine.py
+++ src/app/database/engine.py
@@ -1,11 +1,12 @@
-from accentdatabase.encoders import json_serializer
+import json
+
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from app.config import settings
 
 engine = create_async_engine(
     str(settings.database_url),
-    json_serializer=json_serializer,
+    json_serializer=json.dumps,
     future=True,
     echo=False,
 )
