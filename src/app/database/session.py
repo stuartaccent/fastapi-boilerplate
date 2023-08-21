906bad6bb891fd1d0aead1c2ef94e3f84666d55d
Stuart George <stuart@accentdesign.co.uk>
2023-08-21 12:17:33 +0100

    improve session usage

--- src/app/database/session.py
+++ src/app/database/session.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
@@ -10,24 +11,44 @@ async_session = async_sessionmaker(
 )
 
 
+@asynccontextmanager
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     """
-    returns an async session object.
+    This method is an async context manager that provides an asynchronous SQLAlchemy session.
 
-    Example FastAPI usage:
+    @return: An async generator that yields an asynchronous SQLAlchemy session object.
 
-        from app.database.session import get_session
-        from fastapi import Depends, FastApi
-        from sqlalchemy import select
-        from sqlalchemy.ext.asyncio import AsyncSession
+    Example usage:
+    ```
+    from app.database.session import get_session
 
-        app = FastAPI()
-
-        @app.get("/items")
-        async def items(session: AsyncSession = Depends(get_session)):
-            qs = select(Item)
-            return (await session.execute(qs)).scalars().all()
+    async with get_session() as session:
+        # Use the session object for database operations
+        await session.execute(...)
+    ```
     """
 
     async with async_session() as session:
         yield session
+
+
+async def get_session_dependency() -> AsyncSession:
+    """
+    Get a dependency for an asynchronous SQLAlchemy session.
+
+    @return: An asynchronous SQLAlchemy session.
+    @rtype: sqlalchemy.ext.asyncio.AsyncSession
+
+    Example usage:
+    ```
+    from app.database.session import get_session
+
+    @app.get("")
+    async def endpoint(session: AsyncSession = Depends(get_session_dependency)):
+        # Use the session object for database operations
+        return await session.scalars(...)
+    ```
+    """
+
+    async with get_session() as session:
+        return session
