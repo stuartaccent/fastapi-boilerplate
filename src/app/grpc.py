c9a95ca680d07f8d3e621bbe13e02970e94a6f88
Stuart George <stuart@accentdesign.co.uk>
2023-11-01 17:18:39 +0000

    tidy up grpc service stub

--- src/app/grpc.py
+++ src/app/grpc.py
@@ -13,18 +13,24 @@ class GrpcClientBase:
     on UNAVAILABLE and DEADLINE_EXCEEDED status codes.
     """
 
+    SERVICE_STUB = None
+
     def __init__(
         self,
         host: str,
         port: int,
-        max_retries: int,
-        retry_interval: int,
+        max_retries: int = 3,
+        retry_interval: int = 1,
     ):
         self._host = host
         self._port = port
         self._max_retries = max_retries
         self._retry_interval = retry_interval
         self._channel = grpc.aio.insecure_channel(f"{self._host}:{self._port}")
+        if self.SERVICE_STUB:
+            self.SERVICE_STUB.__init__(self, self._channel)
+        else:
+            raise NotImplementedError("Subclasses should define a SERVICE_STUB.")
 
     async def __aenter__(self) -> "GrpcClientBase":
         return self
@@ -34,13 +40,13 @@ class GrpcClientBase:
 
     async def _retry_on_unavailable(self, call, *args, **kwargs):
         retries = 0
-        error = None
+        last_error = None
         while retries <= self._max_retries:
             try:
                 return await call(*args, **kwargs)
             except grpc.aio.AioRpcError as e:
-                error = e
-                if error.code() in (
+                last_error = e
+                if e.code() in (
                     grpc.StatusCode.UNAVAILABLE,
                     grpc.StatusCode.DEADLINE_EXCEEDED,
                 ):
@@ -48,45 +54,30 @@ class GrpcClientBase:
                     await asyncio.sleep(self._retry_interval)
                 else:
                     raise
-        raise error
+        raise last_error
 
     def __getattribute__(self, name):
-        try:
-            orig_attr = super().__getattribute__(name)
-        except AttributeError:
-            return super().__getattribute__(name)
+        orig_attr = object.__getattribute__(self, name)
+        if not callable(orig_attr) or name.startswith("__"):
+            return orig_attr
 
-        if callable(orig_attr) and not name.startswith("_"):
+        # don't wrap this classes attributes
+        if name in dir(GrpcClientBase):
+            return orig_attr
 
-            async def wrapped_call(*args, **kwargs):
-                return await self._retry_on_unavailable(orig_attr, *args, **kwargs)
+        # wrap calls on the stub classes with retry logic
+        async def wrapped_call(*args, **kwargs):
+            return await self._retry_on_unavailable(orig_attr, *args, **kwargs)
 
-            return wrapped_call
-        return orig_attr
+        return wrapped_call
 
 
 class AuthGrpcClient(GrpcClientBase, AuthenticationStub):
-    def __init__(
-        self,
-        host: str,
-        port: int,
-        max_retries: int = 3,
-        retry_interval: int = 1,
-    ):
-        GrpcClientBase.__init__(self, host, port, max_retries, retry_interval)
-        AuthenticationStub.__init__(self, self._channel)
+    SERVICE_STUB = AuthenticationStub
 
 
 class EmailGrpcClient(GrpcClientBase, EmailServiceStub):
-    def __init__(
-        self,
-        host: str,
-        port: int,
-        max_retries: int = 3,
-        retry_interval: int = 1,
-    ):
-        GrpcClientBase.__init__(self, host, port, max_retries, retry_interval)
-        EmailServiceStub.__init__(self, self._channel)
+    SERVICE_STUB = EmailServiceStub
 
 
 class GrpcClients(TypedDict, total=False):
